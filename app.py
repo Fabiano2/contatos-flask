afc87d77ac149c05cb33110c4d63ebc0aab03783
falves <fabiano.alves@adp.com>
2023-07-09 21:14:38 -0300

    add validations

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contatos.sqlite3'
 db = SQLAlchemy(app)
+app.secret_key = 'teste@123'
 
 class contatos(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -30,9 +31,12 @@ def adiciona_contato():
 
     if request.method == "POST":
         contato = contatos(nome, telefone)
-        db.session.add(contato)
-        db.session.commit()
-        return redirect(url_for('lista'))
+        if not nome or not telefone:
+            flash("Preencha todos os campos", "error")
+        else:
+            db.session.add(contato)
+            db.session.commit()
+            return redirect(url_for('lista'))
     return render_template('adiciona_contato.html')
 
 @app.route('/<int:id>/atualiza_lista', methods=["GET","POST"])
@@ -42,11 +46,21 @@ def atualiza_lista(id):
     if request.method == 'POST':
         nome = request.form.get("nome")
         telefone = request.form.get("telefone")
-        contatos.query.filter_by(id=id).update({"nome":nome, "telefone":telefone})
-        db.session.commit()
-        return redirect(url_for('lista'))
+        if not nome or not telefone:
+            flash("Preencha todos os campos", "error")
+        else:
+            contatos.query.filter_by(id=id).update({"nome":nome, "telefone":telefone})
+            db.session.commit()
+            return redirect(url_for('lista'))
 
     return render_template("atualiza_lista.html", contato=contato)
+
+@app.route('/<int:id>/exclui_contato', methods=["GET"])
+def exclui_contato(id):
+    contato = contatos.query.filter_by(id=id).first()   
+    db.session.delete(contato)
+    db.session.commit()
+    return redirect(url_for('lista'))
 
 if __name__ == "__main__":
     app.run(debug=True)
